04e058353e0e7e4261099e3695f2f7be003fa5e3
Emmanuel Di Pretoro <edipretoro@gmail.com>
2014-08-07 10:05:46 +0200

    * lib/App/PDF/Splitter.pm: add some options to the CLI

--- lib/App/PDF/Splitter.pm
+++ lib/App/PDF/Splitter.pm
@@ -3,10 +3,21 @@ package App::PDF::Splitter;
 use strict;
 use warnings;
 
+use Spreadsheet::Read;
+use Getopt::Long::Descriptive;
+
 __PACKAGE__->run() unless caller();
 
 sub run {
-
+  my ( $options, $usage ) = describe_options(
+    '%c %o',
+    [ 'path|p=s', 'path to the pdftk binary', { default => '/usr/bin/pdftk' } ],
+    [ 'spreadsheet|s=s', 'path to the spreadsheet', { required => 1 } ],
+    [ 'directory|d=s', 'path to the directory where the PDF are stored', { required => 1 } ],
+    [],
+    [ 'help', 'print usage message and exit' ],
+  );
+  print ( $usage->text ), exit if $options->help();
 }
 
 1;
